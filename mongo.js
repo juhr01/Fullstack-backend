ae27fbae775447e3a399f726220b4d990993d136
juhr01 <juho1.riipinen@gmail.com>
2023-01-20 13:43:13 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://phonebook:${password}@phonebookcluster.zpdqbyt.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+if (process.argv.length == 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+  }
+
+if (process.argv.length == 5) {
+    person.save().then(result => {
+        console.log(`added ${person.name} ${person.number} to phonebook`)
+        mongoose.connection.close()
+      })
+}
+
